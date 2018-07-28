c8630595c63bb734797333f2d4e0514cf1af9a97
Mark <mverleg.noreply@gmail.com>
2018-07-28 12:23:16 +0200

    Make macro to create typed vec (most inate methods done)

--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,11 @@
+#![feature(try_reserve)]
+#![feature(shrink_to)]
+#![feature(vec_resize_with)]
+
+use std::alloc::CollectionAllocErr;
+use std::boxed::Box;
 use std::marker::PhantomData;
-use std::ops::Index;
 use std::prelude::v1::Vec;
-
-pub trait ProtoTIVec<T> {
-    fn new() -> Self;
-
-    fn with_capacity(capacity: usize) -> Self;
-
-    // Should only be visible for impl, not for other files / todo
-    fn expose(&self) -> &Vec<T>;
-
-    fn expose_mut(&mut self) -> &mut Vec<T>;
-}
-
-pub trait TIVec<T>: ProtoTIVec<T> {
-    fn get(&self, index: Id<TIVec<T>>) -> Option<&T>;
-    fn pop(&mut self) -> Option<T>;
-    fn push(&mut self, value: T);
-    // most of the other Vec methods...
-}
 
 // TODO @mverleg: is this ?Sized going to be a problem?
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, Default)]
@@ -27,66 +14,153 @@ pub struct Id<Y: ?Sized> {
     index: usize,
 }
 
-impl<T> Id<TIVec<T>> {
-    pub fn new(value: usize) -> Self {
-        Id { index: value, typ: PhantomData }
-    }
+macro_rules! typed_vec {
+    ( $name: ident of $T: ty ) => {
+        #[derive(Debug)]
+        struct $name ( Vec<$T> ) ;
 
-    pub fn from<IU: Into<usize>>(value: IU) -> Self {
-        Id { index: value.into(), typ: PhantomData }
-    }
+        #[allow(dead_code)]
+        impl $name {
+            fn from_vec(data: Vec<$T>) -> Self {
+                $name( data )
+            }
 
-    /// This should not be exposed
-    fn as_usize(&self) -> usize {
-        self.index as usize
-    }
+            #[inline]
+            pub fn new() -> Self {
+                $name( Vec::new() )
+            }
+
+            #[inline]
+            pub fn with_capacity(capacity: usize) -> Self {
+                $name( Vec::with_capacity(capacity) )
+            }
+
+            #[inline]
+            pub fn capacity(&self) -> usize {
+                self.0.capacity()
+            }
+
+            pub fn reserve(&mut self, additional: usize) {
+                self.0.reserve(additional)
+            }
+
+            pub fn reserve_exact(&mut self, additional: usize) {
+                self.0.reserve_exact(additional)
+            }
+
+            pub fn try_reserve(&mut self, additional: usize) -> Result<(), CollectionAllocErr> {
+                self.0.try_reserve(additional)
+            }
+
+            pub fn try_reserve_exact(&mut self, additional: usize) -> Result<(), CollectionAllocErr>  {
+                self.0.try_reserve_exact(additional)
+            }
+
+            pub fn shrink_to_fit(&mut self) {
+                self.0.shrink_to_fit()
+            }
+
+            pub fn shrink_to(&mut self, min_capacity: usize) {
+                self.0.shrink_to(min_capacity)
+            }
+
+            pub fn into_boxed_slice(self) -> Box<[$T]> {
+                self.0.into_boxed_slice()
+            }
+
+            pub fn truncate(&mut self, len: usize) {
+                self.0.truncate(len)
+            }
+
+            #[inline]
+            pub fn as_slice(&self) -> &[$T] {
+                self.0.as_slice()
+            }
+
+            #[inline]
+            pub fn as_mut_slice(&mut self) -> &mut [$T] {
+                self.0.as_mut_slice()
+            }
+
+            #[inline]
+            pub fn swap_remove(&mut self, index: usize) -> $T {
+                self.0.swap_remove(index)
+            }
+
+            pub fn insert(&mut self, index: usize, element: $T) {
+                self.0.insert(index, element)
+            }
+
+            pub fn remove(&mut self, index: usize) -> $T {
+                self.0.remove(index)
+            }
+
+            pub fn retain<F>(&mut self, f: F) where F: FnMut(&$T) -> bool {
+                self.0.retain(f)
+            }
+
+            #[inline]
+            pub fn dedup_by_key<F, K>(&mut self, key: F) where F: FnMut(&mut $T) -> K, K: PartialEq {
+                self.0.dedup_by_key(key)
+            }
+
+            pub fn dedup_by<F>(&mut self, same_bucket: F) where F: FnMut(&mut $T, &mut $T) -> bool {
+                self.0.dedup_by(same_bucket)
+            }
+
+            #[inline]
+            pub fn push(&mut self, value: $T) {
+                self.0.push(value)
+            }
+
+            #[inline]
+            pub fn pop(&mut self) -> Option<$T> {
+                self.0.pop()
+            }
+
+            #[inline]
+            pub fn append(&mut self, other: &mut Self) {
+                self.0.append(&mut other.0)
+            }
+
+            // TODO:
+//            pub fn drain<R>(&mut self, range: R) -> Drain<$T> where R: RangeBounds<usize> {
+//                self.0.drain(range)
+//            }
+
+            #[inline]
+            pub fn clear(&mut self) {
+                self.0.clear()
+            }
+
+            #[inline]
+            pub fn len(&self) -> usize {
+                self.0.len()
+            }
+
+            pub fn is_empty(&self) -> bool {
+                self.0.is_empty()
+            }
+
+            #[inline]
+            pub fn split_off(&mut self, at: usize) -> Self {
+                $name::from_vec(self.0.split_off(at))
+            }
+
+            pub fn resize_with<F>(&mut self, new_len: usize, f: F) where F: FnMut() -> $T {
+                self.0.resize_with(new_len, f)
+            }
+        }
+    };
 }
 
-impl<X, T> TIVec<T> for X where X: ProtoTIVec<T> {
-    fn get(&self, index: Id<TIVec<T>>) -> Option<&T> {
-        self.expose().get(index.as_usize())
-    }
+#[allow(dead_code)]
+typed_vec!(MyVec of i32);
 
-    fn pop(&mut self) -> Option<T> {
-        self.expose_mut().pop()
-    }
-
-    fn push(&mut self, value: T) {
-        self.expose_mut().push(value)
-    }
-}
-
-impl<X, T> Index<Id<TIVec<T>>> for X where X: TIVec<T> {
-    type Output = T;
-
-    fn index(&self, index: Id<TIVec<T>>) -> &<Self as Index<Id<TIVec<T>>>>::Output {
-        self.expose()[index]
-    }
-}
-
-// // // // // // // // // // // // // // //
-
-#[derive(Debug)]
-struct MyVec<T> {
-    data: Vec<T>
-}
-
-impl<T> ProtoTIVec<T> for MyVec<T> {
-    fn new() -> Self {
-        MyVec { data: Vec::new() }
-    }
-
-    fn with_capacity(capacity: usize) -> Self {
-        MyVec { data: Vec::with_capacity(capacity) }
-    }
-
-    fn expose(&self) -> &Vec<T> {
-        &self.data
-    }
-
-    fn expose_mut(&mut self) -> &mut Vec<T> {
-        &mut self.data
-    }
+#[allow(unused_variables, dead_code)]
+fn tmp() {
+    // TODO @mverleg: remove
+    let m = MyVec::new();
 }
 
 #[cfg(test)]
@@ -94,14 +168,22 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_indexing() {
-        let mut v: MyVec<f64> = MyVec::new();
-        v.push(0.0);
-        v.push(1.0);
-        v.push(2.0);
-        let r = v.get(Id::new(1));
-        assert_eq!(&1.0, r.unwrap());
+    fn test_debug() {
+        typed_vec!(TestVec of i32);
+        let mut mv = TestVec::new();
+        assert_eq!("TestVec([])", format!("{:?}", mv));
+        // todo: with elements
+    }
+
+    #[test]
+    fn test_capacity() {
+        typed_vec!(TestVec of i32);
+        let mut mv = TestVec::with_capacity(7);
+        assert_eq!(7, mv.capacity());
+        mv.push(1);
+        mv.push(1);
+        // Note that 'reserve' is relative to current length, not capacity
+        mv.reserve(7);
+        assert!(mv.capacity() >= 9);
     }
 }
-
-
